fbeb9ba00c26df394eb2f4b0358921d9a43cd8c4
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-07-17 00:37:48 -0400

    comments

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const app = express();
 // set up port
 const PORT = process.env.PORT || 3000;
 
-// make sure we can use JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // make public folder static 
 app.use(express.static("public"));
 
-// code added for Mongo Atlas deployment
+// Connect to mongodb
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/workout",
   { 
@@ -25,7 +24,6 @@ mongoose.connect(
   }
 );
 
-    // routes
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
 
