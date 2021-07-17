9a5e92788d85f0eb0cbcf9e88fc6d0e8111ceace
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-07-16 22:11:03 -0400

    Added router.get and router.post

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,21 @@
 const router = require("express").Router();
 const workout = require("../models/workout.js")
+
+router.get("/api/workouts", (req, res) => {
+    console.log("ping?")
+    workout.aggregate([
+        {
+            $addFields: {
+                totalDuration:{$sum: "$exercises.duration"}
+            }
+        }
+    ])
+})
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({}).then(data => {
+        res.json(data)
+    }).catch(err => {
+        res.json(err)
+    })
+})
